8dc77cc566709ec7f8ca6f1c3948c49f677a9e4b
SE130433\Dell <baohqse130433@fpt.edu.vn>
2022-01-14 11:59:29 +0700

    paging user

--- src/main/java/swp391/Swp391ApiApplication.java
+++ src/main/java/swp391/Swp391ApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Swp391ApiApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(Swp391ApiApplication.class, args);
     }
 
--- src/main/java/swp391/dto/user/PagingFormatUserDto.java
+++ src/main/java/swp391/dto/user/PagingFormatUserDto.java
@@ -1,17 +1,24 @@
 package swp391.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import swp391.entity.User;
 
 import java.util.List;
 
 @Data
-public class PagingFormatUserDto {
-    private int pageNumber;
+@AllArgsConstructor
+@NoArgsConstructor
+public class PagingFormatUserDto<T> {
 
-    private int pageSize;
-
-    private Long totalRecordCount;
-
-    private List<User> records;
+    int recordCount;
+    T response;
+//    private int pageNumber;
+//
+//    private int pageSize;
+//
+//    private Long totalRecordCount;
+//
+//    private List<User> records;
 }
