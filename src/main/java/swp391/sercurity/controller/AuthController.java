7f587eda9df1d4f21b672efe80825a68926cb10b
hutu1213 <huytqse130537@fpt.edu.vn>
2022-02-16 10:26:38 +0700

    16/2

--- src/main/java/swp391/sercurity/controller/AuthController.java
+++ src/main/java/swp391/sercurity/controller/AuthController.java
@@ -17,7 +17,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import swp391.entity.User;
 import swp391.repository.RoleRepository;
+import swp391.repository.UserRepository;
 import swp391.sercurity.dto.LoginDto;
 import swp391.sercurity.jwt.JwtUtils;
 
@@ -33,12 +35,14 @@ public class AuthController {
     private final AuthenticationManager authenticationManager;
     private final JwtUtils jwtUtils;
     private RoleRepository roleRepository;
+    private UserRepository userRepository;
 
 
-    public AuthController(AuthenticationManager authManager, JwtUtils jwtUtils, PasswordEncoder encoder,RoleRepository roleRepository) {
+    public AuthController(AuthenticationManager authManager, JwtUtils jwtUtils, PasswordEncoder encoder,RoleRepository roleRepository,UserRepository userRepository) {
         authenticationManager = authManager;
         this.jwtUtils = jwtUtils;
         this.roleRepository=roleRepository;
+        this.userRepository=userRepository;
 
     }
 
@@ -50,6 +54,12 @@ public class AuthController {
 
 //        Authentication auth = null;
 
+        if (!dto.getEmail().matches("^[\\w-\\.]+@fpt[\\.]edu[\\.]vn")) {
+            return ResponseEntity.badRequest().body("Email flow format: ~~~~@fpt.edu.vn");
+        }
+        if(!userRepository.existsById(dto.getEmail())){
+            return ResponseEntity.badRequest().body("Email not exist");
+        }
         try {
             Authentication auth = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(dto.getEmail(), dto.getPassword()));
@@ -63,7 +73,7 @@ public class AuthController {
             logger.debug("{} has been logged in with wrong password: {}", dto.getEmail(), e.getMessage());
         }
 
-        return ResponseEntity.badRequest().body("Invalid email or password");
+        return ResponseEntity.badRequest().body("Invalid password");
     }
 
     public static ResponseEntity<Object> getResponse(Object token, Object role, HttpStatus status) {
