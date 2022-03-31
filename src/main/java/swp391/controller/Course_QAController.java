f994db1c8f7d92c9818772a43398649d8e203360
hutu1213 <huytqse130537@fpt.edu.vn>
2022-03-31 21:22:43 +0700

    Update Course_QAController.java

--- src/main/java/swp391/controller/Course_QAController.java
+++ src/main/java/swp391/controller/Course_QAController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/course-q&a")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class Course_QAController {
     private Course_QAService course_qaService;
     private CourseService courseService;
